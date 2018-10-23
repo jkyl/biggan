bb7f668c0fa21a87b1d32be5667da89bc5d63cf6
Jonathan Kyl <jonathan5kyl@gmail.com>
2018-10-22 18:51:21 -0700

    good settings :)

--- networks.py
+++ networks.py
@@ -8,10 +8,10 @@ def CycleGAN_generator(n_blocks=6):
   for i in range(n_blocks):
     xi = x
     x = conv2d(x, 128, 3)
-    x = conv2d(x, 128, 3, act=None, res=xi)
+    x = conv2d(x, 128, 3, act=False, res=xi)
   x = conv2d(x, 64, 3, s=.5)
   x = conv2d(x, 32, 3, s=.5)
-  out = conv2d(x, 3, 7, norm=None, act='tanh')
+  out = conv2d(x, 3, 7, norm=False, act='tanh')
   return Model(inputs=inp, outputs=out)
 
 def CycleGAN_discriminator(n_layers=4):
@@ -19,6 +19,6 @@ def CycleGAN_discriminator(n_layers=4):
   for i in range(n_layers):
     n = min(512, 2**(i+6))
     s = 2 if i+1<n_layers else 1
-    x = conv2d(x, n, 4, s=s, norm=i, act='lrelu', sn=True)
-  out = conv2d(x, 1, 4, norm=None, act=None, sn=True)
+    x = conv2d(x, n, 4, s=s, norm=False, act='lrelu', sn=True)
+  out = conv2d(x, 1, 4, norm=False, act=False, sn=True)
   return Model(inputs=inp, outputs=out)
