52b54c9d079a831a9f2e8828e77101bb95531474
Jonathan Kyl <jonathan5kyl@gmail.com>
2018-12-03 10:57:43 -0800

    test

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import os
 
 def get_image_data(dirs, image_size, batch_size, n_threads=8):
-  return tf.random.uniform(-1, 1, dtype=tf.float32, shape=(batch_size, image_size, image_size, 3))
+  return tf.random.uniform(minval=-1, maxval=1, dtype=tf.float32, shape=(batch_size, image_size, image_size, 3))
   def read_and_decode(filename):
     return tf.image.decode_image(tf.read_file(tf.print(filename)), channels=3)
   def keep(img):
