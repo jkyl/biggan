adb1c6e69922cd1e8cd85467a0ddbe2d74d18465
Jonathan Kyl <jonathan5kyl@gmail.com>
2019-02-03 23:30:47 -0800

    just an issue with the step dtypes

--- gan.py
+++ gan.py
@@ -53,14 +53,15 @@ def model_fn(features, labels, mode, params):
 
   # every n_D steps, update both networks.
   # otherwise, just update the discriminator
-  G_step, D_step = tf.train.get_global_step(), tf.Variable(0, dtype=tf.int64)
+  G_step = tf.train.get_global_step()
+  D_step = tf.Variable(0, dtype=G_step.dtype)
   only_train_D = tf.cast(tf.mod(D_step, params['n_D']), tf.bool)
   def train_G():
     return G_opt.minimize(L_G, G_step, G.trainable_weights)
   def train_D():
     return D_opt.minimize(L_D, D_step, D.trainable_weights)
   def train_both():
-    return tf.group(train_G(), train_D())
+    return tf.cast(tf.group(train_G(), train_D()), G_step.dtype)
   train_op = tf.cond(only_train_D, train_D, train_both)
 
   # create some tensorboard summaries
