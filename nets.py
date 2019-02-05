f95656b76d4c71f6667c50ed94aca3bb5f878fb9
Jonathan Kyl <jonathan5kyl@gmail.com>
2019-02-05 13:23:12 -0800

    testing cross replica batch norm

--- nets.py
+++ nets.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-import tensorflow as tf
 import numpy as np
 
 from tensorflow.python.keras.layers import *
