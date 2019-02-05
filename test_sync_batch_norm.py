93f4a0a39ac0bdc9382ed521a77481a0bb975a40
Jonathan Kyl <jonathan5kyl@gmail.com>
2019-02-05 12:14:48 -0800

    testing cross replica batch norm

--- test_sync_batch_norm.py
+++ test_sync_batch_norm.py
@@ -4,10 +4,10 @@ from tensorflow.keras.layers import Input
 from sync_batch_norm import SyncBatchNorm
 
 def make_bn_model():
-  x = Input((64, 64, 64, 1))
+  x = Input((64, 64, 1))
   y = SyncBatchNorm()(x)
   return Model(inputs=x, outputs=y)
 
 if __name__ == '__main__':
   model = make_bn_model()
-  model.predict(np.ones((64, 64, 64, 1)))
+  model.predict(np.ones((1, 64, 64, 1)))
