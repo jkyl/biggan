a30885142252ced96e4777d42dd44fb329e8b09f
Jonathan Kyl <jonathan5kyl@gmail.com>
2019-02-05 12:04:16 -0800

    testing cross replica batch norm

---
+++ test_sync_batch_norm.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model, Input
+from sync_batch_norm import SyncBatchNorm
+
+def make_bn_model():
+  x = Input((64, 64, 64, 1))
+  y = SyncBatchNorm()(x)
+  return Model(inputs=x, outputs=y)
