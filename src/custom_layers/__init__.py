cd12165054965c7453e9b1d59c51d181cb6c839b
Jonathan Kyl <jonathan5kyl@gmail.com>
2019-02-08 12:12:52 -0800

    imports

--- src/custom_layers/__init__.py
+++ src/custom_layers/__init__.py
@@ -1,3 +1,4 @@
-from sn import ConvSN2D, DenseSN
-from bn import SyncBatchNorm
-from up import UnPooling2D
+from __future__ import absolute_import
+from .sn import ConvSN2D, DenseSN
+from .bn import SyncBatchNorm
+from .up import UnPooling2D
